9a890ed825fcfaaf782bf81f7c8583af5ddf56be
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2024-09-11 21:01:43 -0300

    secury

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -68,10 +68,7 @@ public class Principal {
     }
 
     private void listarSeriesBuscadas() {
-        List<Serie> series = new ArrayList<>();
-        series = dadosSeries.stream()
-                        .map(d -> new Serie(d))
-                                .collect(Collectors.toList());
+        List<Serie> series = repository.findAll();
 
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
