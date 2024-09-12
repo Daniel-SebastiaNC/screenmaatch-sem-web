6415c6af871e36900b0bdad87855a20b2d0d28cb
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2024-09-12 16:50:48 -0300

    filter year

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -44,6 +44,7 @@ public class Principal {
             8 - Filtrar séries
             9 - Buscar episódios por trecho
             10 - Top 5 episódios 
+            11 - Buscar episódios a partir de uma data
 
             0 - Sair                                 
             """;
@@ -82,6 +83,9 @@ public class Principal {
                     break;
                 case 10:
                     top5Eps();
+                    break;
+                case 11:
+                    buscarEpisodiosDepoisDeUmaData();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -227,6 +231,19 @@ public class Principal {
                     System.out.printf("Série: %s Temporada %s - Episódio %s - %s Avaliação %s\n",
                             e.getSerie().getTitulo(), e.getTemporada(),
                             e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao()));
+        }
+    }
+
+    private void buscarEpisodiosDepoisDeUmaData() {
+        buscarSeriePorTitulo();
+        if (serieBusca.isPresent()) {
+            Serie serie = serieBusca.get();
+            System.out.println("Digite o ano limite de lançamento");
+            var anoLancamento = scanner.nextInt();
+            scanner.nextLine();
+
+            List<Episodio> episodiosAno = repository.episodiosPorSerieEAno(serie, anoLancamento);
+            episodiosAno.forEach(System.out::println);
         }
     }
 }
